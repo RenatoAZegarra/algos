a488a25d42da519a07c929e76995f26609b0e474
RenatoAZegarra <Zegarrarenatoa@gmail.com>
2023-04-28 11:40:23 -0400

    3rd commit

---
+++ algo_w4d4.js
@@ -0,0 +1,40 @@
+/*
+  Sum To One Digit
+  Implement a function sumToOne(num)​ that,
+  given a number, sums that number’s digits
+  repeatedly until the sum is only one digit. Return
+  that final one digit result.
+  Tips:
+    to access digits from a number, need to convert it .toString() to access each digit via index
+    parseInt(arg) returns arg parsed as an integer, or NaN if it couldn't be converted to an int
+    isNaN(arg) used to check if something is NaN
+*/
+
+const num1 = 5;
+const expected1 = 5;
+
+const num2 = 10;
+const expected2 = 1;
+
+const num3 = 25;
+const expected3 = 7;
+
+const num4 = 487;
+const expected4 = 1;
+
+/**
+ * Sums the given number's digits until the number becomes one digit.
+ * @param {number} num The number to sum to one digit.
+ * @returns {number} One digit.
+ */
+function sumToOneDigit(num) {
+  if (num < 10){
+    return num;
+  }
+  // grab the remainder
+  var remainder = num % 10; 
+  //grab the other nums by dividing it by 10
+  var otherNums = Math.floor(num / 10);
+  return sumToOneDigit(remainder + otherNums);
+}
+console.log(sumToOneDigit(num4));
---
+++ algo_w4d5.js
@@ -0,0 +1,95 @@
+/* 
+  String Anagrams
+  Given a string,
+  return array where each element is a string representing a different anagram (a different sequence of the letters in that string).
+  Ok to use built in methods
+*/
+
+const str1 = "lim";
+const expected1 = ["ilm", "iml", "lim", "lmi", "mil", "mli"];
+// Order of the output array does not matter
+
+/**
+ * Add params if needed for recursion.
+ * Generates all anagrams of the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {Array<string>} All anagrams of the given str.
+ */
+function generateAnagrams(str, startIndex = 0, anagrams = []) {
+  if (startIndex === str.length - 1) {
+    // Base case: no more characters to swap, add permutation to anagrams array
+    anagrams.push(str);
+  } else {
+    // Recursive case: generate all possible permutations by swapping characters
+    for (let i = startIndex; i < str.length; i++) {
+      const strArr = str.split("");
+      [strArr[startIndex], strArr[i]] = [strArr[i], strArr[startIndex]]; // swap characters
+      generateAnagrams(strArr.join(""), startIndex + 1, anagrams); // generate permutations of remaining characters
+    }
+  }
+  return anagrams;
+}
+const actual1 = generateAnagrams(str1).sort();
+console.log(actual1);
+
+
+/* 
+  Recursively find the lowest common multiple between two numbers
+  "A common multiple is a number that is a multiple of two or more integers. 
+  The common multiples of 3 and 4 are 0, 12, 24, .... 
+  The least common multiple (LCM) of two numbers is the smallest
+  number (not zero) that is a multiple of both."
+  
+  Try writing two columns of multiples as a starting point:
+  starting with 15 and 25 and keep writing their multiples until you find the
+  lowest common one then turn this in to a step by step instruction
+  15 25
+  30 50
+  45 75
+  60 
+  75
+  75 is the first common
+*/
+
+const num1A = 1;
+const num1B = 1;
+// const expected1 = 1;
+
+const num2A = 5;
+const num2B = 10;
+const expected2 = 10;
+
+const num3A = 10;
+const num3B = 5;
+const expected3 = 10;
+
+const num4A = 6;
+const num4B = 8;
+const expected4 = 24;
+
+const num5A = 15;
+const num5B = 25;
+const expected5 = 75;
+
+/**
+ * Add params if needed for recursion
+ * Finds the lowest common multiple of the two given ints.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} The lowest common multiple of the given ints.
+ */
+function lowestCommonMultiple(a, b, product = a) {
+  if (a === 0 || b === 0) {
+    return 0;
+  }
+  if (a === b) {
+    return a;
+  }
+  if (product % b === 0) {
+    return product;
+  }
+  return lowestCommonMultiple(a, b, product + a);
+}
+console.log(lowestCommonMultiple(num5A,num5B))
